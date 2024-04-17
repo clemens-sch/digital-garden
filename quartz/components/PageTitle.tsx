33ff18a4e5c5c4a5e44efb9740300915c0845acf
clemens-sch <schclemens05@gmail.com>
2024-04-17 11:37:14 +0200

    Quartz sync: Apr 17, 2024, 11:37 AM

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -4,7 +4,8 @@ import { classNames } from "../util/lang"
 import { i18n } from "../i18n"
 
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
-  const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
+  // const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
+  const title = "Second-Brain"
   const baseDir = pathToRoot(fileData.slug!)
   return (
     <h1 class={classNames(displayClass, "page-title")}>
