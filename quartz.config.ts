34124363581e4ee5891883f5d3173691221f4a3a
clemens-sch <schclemens05@gmail.com>
2024-09-16 11:30:36 +0200

    Quartz sync: Sep 16, 2024, 11:30 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -42,7 +42,7 @@ const config: QuartzConfig = {
           lightgray: "#393639",
           gray: "#646464",
           darkgray: "#d4d4d4",
-          dark: "#cecece",
+          dark: "#ebebec",
           secondary: "#7b97aa",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
